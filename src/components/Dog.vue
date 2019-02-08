7bd27c92bb4b2be7c6bac31ce095f876f5947afd
heliomarcondes <helio@prodweb.com.br>
2019-02-07 22:20:48 -0200

    Add project

---
+++ src/components/Dog.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-card color="grey lighten-2">
+            <v-img :src="dog.img" height="170px"> </v-img>
+            <v-card-title>
+                <div>
+                <h3>{{dog.name}}</h3>
+                <p class="breed">{{dog.breed}}</p>
+                </div>
+            </v-card-title>
+            <v-btn class="light" block @click="$emit('addToFavorites', dog)"><v-icon color="red">favorite</v-icon></v-btn>
+    </v-card>
+</template>
+
+<script>
+	export default {
+	  props: {
+	    dog: {
+	      type: Object
+	    }
+	  }
+	};
+</script>
+
+<style lang="scss" scoped>
+.light{
+  box-shadow:none !important;
+}
+.v-btn{
+  height: 52px;
+}
+.v-card__title{
+  background:#fff;
+}
+.v-card{
+  margin-top: 0 !important;
+  box-shadow: none !important;
+  border-radius: 3px;
+    background-color: #ffffff;
+    box-shadow: 0px 5px 25px 0px rgba(46, 61, 73, 0.2) !important;
+    transition: all 0.5s ease;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import VueRouter from 'vue-router';
+import Home from './views/Home';
+import Pets from './views/Pets';
+import Favorites from "./views/Favorites";
+import Form from "./views/Form";
+import store from "./store";
+
+Vue.use(Vuetify);
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/pets', component: Pets },
+  { path: "/favorites", component: Favorites },
+  { path: "/form", component: Form }
+];
+
+const router = new VueRouter({ routes });
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  store
 }).$mount('#app')
